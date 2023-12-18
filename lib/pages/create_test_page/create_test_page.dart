32e1296827bb939ffe6114506f77d61526ef9377
netos23 <nikmoroz2002@outlook.com>
2023-12-19 01:08:47 +0300

    feature(pages) add menu, history, tests, create_test pages

---
+++ lib/pages/create_test_page/create_test_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateTestPage extends StatelessWidget {
+  const CreateTestPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
