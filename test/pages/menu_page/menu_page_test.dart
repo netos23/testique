954d4e8fbd7cdeae6e10a00c4b09bb83ab9f1b7f
netos23 <nikmoroz2002@outlook.com>
2023-12-19 01:50:56 +0300

    test(menu) add layout tests

---
+++ test/pages/menu_page/menu_page_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:testique/pages/menu_page/menu_page.dart';
+import 'package:testique/resources/res.dart';
+import 'package:testique/testique_app.dart';
+
+void main() {
+  group('MenuPage content tests', () {
+
+    testWidgets('Menu page text contains', (WidgetTester tester) async {
+      await tester.pumpWidget(const MenuPage());
+
+      expect(find.text('Создать тест'), findsOneWidget);
+      expect(find.text('Решить тест'), findsOneWidget);
+      expect(find.text('Импорт теста'), findsOneWidget);
+      expect(find.text('История тестов'), findsOneWidget);
+    });
+
+    testWidgets('Menu page images contains', (WidgetTester tester) async {
+      await tester.pumpWidget(const MenuPage());
+
+      expect(find.image(const AssetImage(AppIcons.brain)), findsOneWidget);
+      expect(find.image(const AssetImage(AppIcons.edit)), findsOneWidget);
+      expect(find.image(const AssetImage(AppIcons.history)), findsOneWidget);
+      expect(find.image(const AssetImage(AppIcons.import)), findsOneWidget);
+    });
+
+  });
+}
