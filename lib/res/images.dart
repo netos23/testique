6bd77bc78ca491842129b53ad4eb83d3f0677aa0
netos23 <nikmoroz2002@outlook.com>
2023-12-18 23:45:51 +0300

    feature(assets) add images

---
+++ lib/res/images.dart
@@ -0,0 +1,8 @@
+final class AppIcons{
+  AppIcons._();
+
+  static const brain = 'assets/images/brain.png';
+  static const edit = 'assets/images/edit.png';
+  static const history = 'assets/images/history.png';
+  static const import = 'assets/images/import.png';
+}
