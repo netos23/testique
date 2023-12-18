0441a33d87afe0162b3791d7c2c084eb6ef1b773
netos23 <nikmoroz2002@outlook.com>
2023-12-18 23:57:06 +0300

    feature(root) add text theme

--- lib/testique_app.dart
+++ lib/testique_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:testique/res/colors.dart';
 
 class TestiqueApp extends StatelessWidget {
@@ -14,6 +15,7 @@ class TestiqueApp extends StatelessWidget {
         ).copyWith(
           background: AppColors.background,
         ),
+        textTheme: GoogleFonts.montserratTextTheme(),
         useMaterial3: false,
       ),
       home: Container(),
